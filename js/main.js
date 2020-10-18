22a626b925c81f12ef3932614279deaf567e1fdd
ppopow <pawel.popow@gmail.com>
2020-10-18 20:40:57 +0200

    nowe zadanie 34

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let text='JavaScript';
+let poem='Jestem świetnym programistą';
+
+function text2(w, k) {
+    let d;
+    if(w>k) {
+        d=w;
+    } else {
+        d=k;
+    } 
+}
+
+console.log(text, poem);
